71dfb368d43dac1251c09ce775e874a5be623941
SafinatunNaja29 <safinatun.nj29@gmail.com>
2023-05-08 15:00:36 +0700

    object name

--- 184_Pointer/184_Pointer.cpp
+++ 184_Pointer/184_Pointer.cpp
@@ -11,3 +11,13 @@ public:
 void Mahasiswa::showNim() {		// implementasi method diluar class
 	cout << " No Induk = " << nim << endl;
 }
+
+int main()
+{
+	Mahasiswa mhs{ 1 };		// object name
+	mhs.showNim();			// member access operator
+
+	Mahasiswa& ref = mhs;	// pointer reference refMhs
+	ref.nim = 2;			// member access operator
+	mhs.showNim();
+}
