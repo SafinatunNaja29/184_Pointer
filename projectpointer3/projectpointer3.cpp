511249925894b6cb34b7295612567e9be1a0f372
SafinatunNaja29 <safinatun.nj29@gmail.com>
2023-05-08 15:34:57 +0700

    void siswa dan orang

--- projectpointer3/projectpointer3.cpp
+++ projectpointer3/projectpointer3.cpp
@@ -10,3 +10,25 @@ public:
 	void setNama(string pNama);
 	friend class siswa;
 };
+
+class siswa {
+private:
+	int Id;
+public:
+	void setId(int pId);
+	void displayAll(orang& a);
+};
+
+
+void siswa::displayAll(orang& a) {
+	cout << Id << endl << a.nama;
+}
+
+void siswa::setId(int pId) {
+	Id = pId;
+}
+
+void orang::setNama(string pNama) {
+	nama = pNama;
+}
+
